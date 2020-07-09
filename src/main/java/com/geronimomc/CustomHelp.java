63645ca0422c856c69322af1277a6558b9f6bb5e
Simon_Xz <64443757+Simon-Xz@users.noreply.github.com>
2020-07-09 01:34:32 +0100

    started wiork on hovertext ect

--- src/main/java/com/geronimomc/CustomHelp.java
+++ src/main/java/com/geronimomc/CustomHelp.java
@@ -1,10 +1,14 @@
 package com.geronimomc;
 
+import jdk.nashorn.internal.runtime.regexp.joni.encoding.CharacterType;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+
+import java.awt.*;
 
 public class CustomHelp implements Listener, CommandExecutor {
 
@@ -12,9 +16,17 @@ public class CustomHelp implements Listener, CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
             Player p = (Player) sender;
-            p.sendMessage("Testing");
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6&lGeronimoMC"));
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eClick to select a help option..."));
+            p.sendMessage(bug);
+            p.sendMessage(rules);
+            p.sendMessage(payment);
+
         }
-        return false;
+
+        return true;
     }
+
+
 
 }
