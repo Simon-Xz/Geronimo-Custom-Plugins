271e5f04139b278f2337e21e1b1442d1bc244c3b
Simon_Xz <64443757+Simon-Xz@users.noreply.github.com>
2020-07-29 20:17:58 +0100

    Fixed Leaderboard
    Leaderboard wasnt displaying online & offline players correctly

--- src/main/java/com/geronimomc/leaderboard/PrestigeTop.java
+++ src/main/java/com/geronimomc/leaderboard/PrestigeTop.java
@@ -28,30 +28,31 @@ public class PrestigeTop  implements CommandExecutor, Listener {
 
         TreeMap<Integer, String> map = new TreeMap<Integer, String>();
         for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-            Integer prestige = Main.cfgm.getPlayers().getInt("Players." + p.getUniqueId().toString() + ".Prestige");
-            if (Main.cfgm.getPlayers().contains("Players." + p.getUniqueId().toString() + ".Prestige"))
+            Integer prestige = Main.cfgm.getPlayers().getInt("Players." + onlinePlayer.getUniqueId().toString() + ".Prestige");
+            if (Main.cfgm.getPlayers().contains("Players." + onlinePlayer.getUniqueId().toString() + ".Prestige"))
                 map.put(prestige, onlinePlayer.getName());
         }
         for (OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers()) {
             if (offlinePlayer != null && offlinePlayer.getName() != null) {
-                Integer prestige = Main.cfgm.getPlayers().getInt("Players." + p.getUniqueId().toString() + ".Prestige");
-                if (Main.cfgm.getPlayers().contains("Players." + p.getUniqueId().toString() + ".Prestige"))
+                Integer prestige = Main.cfgm.getPlayers().getInt("Players." + offlinePlayer.getUniqueId().toString() + ".Prestige");
+                if (Main.cfgm.getPlayers().contains("Players." + offlinePlayer.getUniqueId().toString() + ".Prestige"))
                     map.put(prestige, offlinePlayer.getName());
             }
         }
         p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6&lTOP 5 PRESTIGE"));
         int n = 1;
 
-            NavigableMap<Integer, String> nmap = map.descendingMap();
-            for (NavigableMap.Entry<Integer, String> entry : nmap.entrySet()) {
-                p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e#"+n+". &f" + entry.getValue() + " &8-&b Prestige " + entry.getKey()));
-                n++;
-                if (n == 6) {
-                    break;
-                }
+        NavigableMap<Integer, String> nmap = map.descendingMap();
+        for (NavigableMap.Entry<Integer, String> entry : nmap.entrySet()) {
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e#"+n+". &f" + entry.getValue() + " &8-&b Prestige " + entry.getKey()));
+            n++;
+            if (n == 6) {
+                break;
             }
-
         }
+
+    }
+
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(sender instanceof Player) {
             Player p = (Player)sender;
