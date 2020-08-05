3d494887fcb22dec1a1fdfa9a0ab24ad31a28616
Simon_Xz <64443757+Simon-Xz@users.noreply.github.com>
2020-08-05 10:53:42 +0100

    Fixed pickaxe login events

--- src/main/java/com/geronimomc/UserLoginEvents.java
+++ src/main/java/com/geronimomc/UserLoginEvents.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
@@ -28,25 +29,30 @@ public class UserLoginEvents implements Listener {
             p.sendMessage(ChatColor.translateAlternateColorCodes('&', "  &8>> &fThere are currently &6" + Bukkit.getOnlinePlayers().size() + "&e/&6" + Bukkit.getMaxPlayers() + "&f players online!"));
             p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&m&l--------------------------------------------------"));
         }
-        if (!p.getInventory().contains(new ItemStack(Material.DIAMOND_PICKAXE))) {
+        if (!p.getInventory().contains(Material.DIAMOND_PICKAXE)) {
             if(p.hasPermission("core.rank.default")) {
                 ItemStack pickaxe = new ItemStack(Material.DIAMOND_PICKAXE);
                 ItemMeta pickaxeMeta = pickaxe.getItemMeta();
+
+                // SETTING PICKAXE NAME AND ENCHANTS
                 pickaxeMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&7&lMember"));
                 pickaxeMeta.addEnchant(Enchantment.DIG_SPEED,15, true);
                 pickaxeMeta.addEnchant(Enchantment.LOOT_BONUS_BLOCKS,15, true);
                 pickaxeMeta.addEnchant(Enchantment.DURABILITY, 15, true);
-                pickaxeMeta.isUnbreakable();
+                pickaxeMeta.setUnbreakable(true);
                 pickaxeMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
                 pickaxeMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+                pickaxeMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
 
+                // SETTING PICKAXE LORE
                 ArrayList<String> pickaxeLore = new ArrayList<String>();
                 pickaxeLore.add(ChatColor.translateAlternateColorCodes('&', "&6Item Info:"));
-                pickaxeLore.add(ChatColor.RED + "Omnitool");
-                pickaxeLore.add(ChatColor.RED + "Unbreakable");
+                pickaxeLore.add(ChatColor.RED + " Omnitool");
+                pickaxeLore.add(ChatColor.RED + " Unbreakable");
                 pickaxeLore.add(" ");
                 pickaxeLore.add(ChatColor.GOLD + "Enchants:");
                 pickaxeLore.add(ChatColor.translateAlternateColorCodes('&', " &7Efficiency " + pickaxeMeta.getEnchantLevel(Enchantment.DIG_SPEED)));
+                pickaxeLore.add(ChatColor.translateAlternateColorCodes('&', " &7Fortune " + pickaxeMeta.getEnchantLevel(Enchantment.LOOT_BONUS_BLOCKS)));
                 pickaxeMeta.setLore(pickaxeLore);
 
                 pickaxe.setItemMeta(pickaxeMeta);
@@ -54,5 +60,10 @@ public class UserLoginEvents implements Listener {
                 p.getInventory().setItem(1, pickaxe);
             }
         }
+    }
+
+    @EventHandler
+    public void onToolUse(BlockBreakEvent e) {
+        e.getPlayer().getItemOnCursor().setDurability((short) -10);
     }
 }
